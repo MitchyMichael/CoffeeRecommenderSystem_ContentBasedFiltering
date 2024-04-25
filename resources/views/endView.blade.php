62ae345201f93e4642e508fe2cef3222142bff87
Michael Wijaya Sutrisna <michaelwijayasutrisna2002@gmail.com>
2024-04-25 18:54:20 +0700

    Add: End View Design

--- resources/views/endView.blade.php
+++ resources/views/endView.blade.php
@@ -1,22 +1,30 @@
 @extends('layout.layout')
 @section('main_content')
     <div class="container">
-        <div class="endMain">
-            <h1>Thank You!</h1>
-            <p class="recSubTitle">
-                Please tell the cashier you ordered this menu!
-            </p>
+        <div class="thisContainer">
+            <div class="endMain">
+                <h1>Thank You!</h1>
+                <p class="recSubTitle">
+                    Please tell the cashier you ordered this menu!
+                </p>
 
-            <div class="endMenu">
-                <?php
-                use App\Models\Choise;
-                $thisCustomerId = $customerId;
+                <div class="endMenu">
+                    <?php
+                    use App\Models\Choise;
+                    use App\Models\Coffee;
 
-                $choise = Choise::findOrFail($thisCustomerId);
-                $coffeeId = $choise->coffee_id;
-                ?>
-                
-                <p>{{ $coffeeId }}</p>
+                    $thisCustomerId = $customerId;
+                    $choise = Choise::findOrFail($thisCustomerId);
+                    $coffeeId = $choise->coffee_id;
+
+                    $coffee = Coffee::findOrFail($coffeeId);
+                    ?>
+
+                    <h2>{{ $coffee->coffeeName }}</h2>
+                    <img src="{{ $coffee->coffeePhoto }}">
+                    <p>{{ $coffee->coffeeDescription }}</p>
+                    <p><strong>Rp {{ $coffee->coffeePrice }}</strong></p>
+                </div>
             </div>
         </div>
     </div>
