9157387d958eca1167ef63b8ef85d3a7c92b0659
Michael Wijaya Sutrisna <michaelwijayasutrisna2002@gmail.com>
2024-04-30 13:40:31 +0700

    Update LikeController.php

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -9,11 +9,13 @@ class LikeController extends Controller
 {
     public function saveLike(Request $request){
         $customerId = $request->input('customer_id');
-        dd($customerId);
+        // dd($customerId);
 
         $question1 = $request->input('question1');
         $question2 = $request->input('question2');
         $question3 = $request->input('question3');
+
+        dd($question1, $question2, $question3);
 
         $choise = Choise::findOrFail($customerId);
         $choise->question1 = $question1;
