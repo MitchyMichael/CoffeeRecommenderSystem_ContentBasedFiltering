44ded83acfbe17e46a9141b4dd32169c13c30828
Michael Wijaya Sutrisna <michaelwijayasutrisna2002@gmail.com>
2024-05-31 09:25:14 +0700

    Refactor: Admin design

--- resources/views/addNewCoffeeView.blade.php
+++ resources/views/addNewCoffeeView.blade.php
@@ -27,50 +27,132 @@
 
     <div class="container">
         <div class="addCoffeeForm">
-            <br><br>
+            <br>
             <h1>Tambah Menu Kopi</h1><br>
             <h1 style="display:none;">Cafe ID: <span id="cafeIdDisplay"></span></h1>
+
             <form action="/submitNewCoffee" method="POST" enctype="multipart/form-data">
                 @csrf
-                <label for="coffeeName">Nama menu:</label> <input type="text" id="coffeeName" name="coffeeName" value="Menu Baru"><br>
+                {{-- Nama Menu --}}
+                <div class="form-group row">
+                    <label for="coffeeName" class="col-sm-2 col-form-label">Nama menu</label><br>
+                    <div class="col-sm-10">
+                        <input type="text" id="coffeeName" name="coffeeName" value="Menu Baru" class="form-control">
+                    </div>
+                </div>
+                <p></p>
 
-                <label for="coffeeDescription">Deskripsi Menu:</label> <input type="text" id="coffeeDescription" name="coffeeDescription" value="Deskripsi Menu"><br>
+                {{-- Deskripsi Menu --}}
+                <div class="form-group row">
+                    <label for="coffeeDescription" class="col-sm-2 col-form-label">Deskripsi Menu</label><br>
+                    <div class="col-sm-10">
+                        <input type="text" id="coffeeDescription" name="coffeeDescription" value="Deskripsi Menu"
+                            class="form-control">
+                    </div>
+                </div>
+                <p></p>
 
-                <label for="coffeePrice">Harga Menu:</label> <input type="number" id="coffeePrice" name="coffeePrice" value="20000">
+                {{-- Harga Menu --}}
+                <div class="form-group row">
+                    <label for="coffeePrice" class="col-sm-2 col-form-label">Harga Menu</label>
+                    <div class="col-sm-10">
+                        <input type="number" id="coffeePrice" name="coffeePrice" value="20000" class="form-control">
+                    </div>
+                </div>
+                <hr>
 
-                <br>
+                <input type="hidden" id="coffeeIsBestSeller" name="coffeeIsBestSeller" value="true">
 
-                <input type="hidden" id="coffeeIsBestSeller" name="coffeeIsBestSeller" value="true"><br>
+                {{-- Mood --}}
+                <div class="form-group row">
+                    <label for="coffeePreferenceMood" class="col-sm-2 col-form-label">Angka Preferensi Mood
+                        (1-5(Baik))</label>
+                    <div class="col-sm-10">
+                        <input type="number" id="coffeePreferenceMood" name="coffeePreferenceMood" min="1"
+                            max="5" value="0" class="form-control">
+                    </div>
+                </div>
 
-                <label for="coffeePreferenceMood">Angka Preferensi Mood (1-5(Baik)):</label> <input type="number" id="coffeePreferenceMood" name="coffeePreferenceMood" min="1"
-                    max="5" value="0"><br>
+                {{-- Activity --}}
+                <div class="form-group row">
+                    <label for="coffeePreferenceActivity" class="col-sm-2 col-form-label">Angka Preferensi Kegiatan
+                        (1(Bersantai) atau 5(Melakukan
+                        sesuatu))</label>
+                    <div class="col-sm-10">
+                        <input type="number" id="coffeePreferenceActivity" name="coffeePreferenceActivity" min="1"
+                            max="5" value="0" class="form-control">
+                    </div>
+                </div>
 
-                <label for="coffeePreferenceActivity">Angka Preferensi Kegiatan (1(Bersantai) atau 5(Melakukan sesuatu)):</label> <input type="number" id="coffeePreferenceActivity" name="coffeePreferenceActivity" min="1"
-                    max="5" value="0"><br>
+                {{-- Temperature --}}
+                <div class="form-group row">
+                    <label for="coffeeTemperature" class="col-sm-2 col-form-label">Suhu Menu (1(Dingin) atau
+                        5(Panas))</label>
+                    <div class="col-sm-10">
+                        <input type="number" id="coffeeTemperature" name="coffeeTemperature" min="1" max="5"
+                            value="0" class="form-control">
+                    </div>
+                </div>
 
-                <label for="coffeeTemperature">Suhu Menu (1(Dingin) atau 5(Panas)):</label> <input type="number" id="coffeeTemperature" name="coffeeTemperature" min="1" max="5" value="0"><br>
+                {{-- Sweetness --}}
+                <div class="form-group row">
+                    <label for="coffeeSweetness" class="col-sm-2 col-form-label">Angka Tingkat Kemanisan (1-5(Sangat
+                        Manis))</label>
+                    <div class="col-sm-10">
+                        <input type="number" id="coffeeSweetness" name="coffeeSweetness" min="1" max="5"
+                            value="0" class="form-control">
+                    </div>
+                </div>
 
-                <label for="coffeeSweetness">Angka Tingkat Kemanisan (1-5(Sangat Manis)):</label> <input type="number" id="coffeeSweetness" name="coffeeSweetness" min="1" max="5" value="0"><br>
+                {{-- Milk --}}
+                <div class="form-group row">
+                    <label for="coffeeMilkness" class="col-sm-2 col-form-label">Penggunaan Susu (1(Tanpa Susu) atau 5(Dengan
+                        Susu))</label>
+                    <div class="col-sm-10">
+                        <input type="number" id="coffeeMilkness" name="coffeeMilkness" min="1" max="5"
+                            value="0" class="form-control">
+                    </div>
+                </div>
 
-                <label for="coffeeMilkness">Penggunaan Susu (1(Tanpa Susu) atau 5(Dengan Susu)):</label> <input type="number" id="coffeeMilkness" name="coffeeMilkness" min="1" max="5" value="0"><br>
+                {{-- Price --}}
+                <div class="form-group row">
+                    <label for="coffeeCheapness" class="col-sm-2 col-form-label">Angka Tingkat Harga (1-5(Lebih
+                        Mahal))</label>
+                    <div class="col-sm-10">
+                        <input type="number" id="coffeeCheapness" name="coffeeCheapness" min="1" max="5"
+                            value="0" class="form-control">
+                    </div>
+                </div>
 
-                <label for="coffeeCheapness">Angka Tingkat Harga (1-5(Lebih Mahal)):</label> <input type="number" id="coffeeCheapness" name="coffeeCheapness" min="1" max="5" value="0"><br>
+                {{-- Acid --}}
+                <div class="form-group row">
+                    <label for="coffeeAcidityLevel" class="col-sm-2 col-form-label">Angka Tingkat Keasaman
+                        (1-5(Asam))</label>
+                    <div class="col-sm-10">
+                        <input type="number" id="coffeeAcidityLevel" name="coffeeAcidityLevel" min="1"
+                            max="5" value="0" class="form-control">
+                    </div>
+                </div>
 
-                <label for="coffeeAcidityLevel">Angka Tingkat Keasaman (1-5(Asam)):</label> <input type="number" id="coffeeAcidityLevel" name="coffeeAcidityLevel" min="1" max="5" value="0"><br>
+                {{-- Strength --}}
+                <div class="form-group row">
+                    <label for="coffeeStrengthLevel" class="col-sm-2 col-form-label">Angka Tingkat Kekuatan
+                        (1-5(Kuat))</label>
+                    <div class="col-sm-10">
+                        <input type="number" id="coffeeStrengthLevel" name="coffeeStrengthLevel" min="1"
+                            max="5" value="0" class="form-control">
+                    </div>
+                </div>
+                <hr>
 
-                <label for="coffeeStrengthLevel">Angka Tingkat Kekuatan (1-5(Kuat)):</label> <input type="number" id="coffeeStrengthLevel" name="coffeeStrengthLevel" min="1" max="5" value="0">
-
-                <br><br>
-
-                <label for="coffeePhoto">Foto Menu:</label><br>
-                <input type="file" id="coffeePhoto" name="coffeePhoto" accept="image/*">
-
-                <br>
+                <div class="custom-file">
+                    <label for="coffeePhoto" class="custom-file-label col-sm-2 col-form-label">Foto Menu</label>
+                    <input type="file" id="coffeePhoto" name="coffeePhoto" accept="image/*" class="custom-file-input">
+                </div>
 
                 <input type="hidden" id="cafeIdField" name="cafeIdField">
 
                 <br>
-
                 <input type="submit" value="Simpan" class="btn btn-primary">
                 <br><br><br>
             </form>
--- resources/views/editCoffeeView.blade.php
+++ resources/views/editCoffeeView.blade.php
@@ -1,53 +1,145 @@
 @extends('layout.layout')
 @section('main_content')
-<div class="container">
-    <div class="addCoffeeForm">
-        <br><br>
-        <h1>Edit Coffee</h1><br>
-        <form action="{{ route('coffee.update', $coffee->id) }}" method="POST" enctype="multipart/form-data">
-            @csrf
-            @method('PUT')
-
-            <input type="hidden" id="cafeId" name="cafeId" value={{ $coffee->cafeId }}><br>
-
-            <label for="coffeeName">Nama Menu:</label> <input type="text" id="coffeeName" name="coffeeName" value={{ $coffee->coffeeName }}><br>
-
-            <label for="coffeeDescription">Deskripsi Menu:</label> <input type="text" id="coffeeDescription" name="coffeeDescription" value={{ $coffee->coffeeDescription }}><br>
-
-            <label for="coffeePrice">Harga Menu:</label> <input type="number" id="coffeePrice" name="coffeePrice" value={{ $coffee->coffeePrice }}><br>
-
-            <input type="hidden" id="coffeeIsBestSeller" name="coffeeIsBestSeller" value="true"><br>
-
-            <label for="coffeePreferenceMood">Angka Preferensi Mood (1-5(Baik)):</label> <input type="number" id="coffeePreferenceMood" name="coffeePreferenceMood" min="1"
-                max="5" value={{ $coffee->coffeePreferenceMood }}><br>
-
-            <label for="coffeePreferenceActivity">Angka Preferensi Kegiatan (1(Bersantai) atau 5(Melakukan sesuatu)):</label> <input type="number" id="coffeePreferenceActivity" name="coffeePreferenceActivity" min="1"
-                max="5" value={{ $coffee->coffeePreferenceActivity }}><br>
-
-            <label for="coffeeTemperature">Suhu Menu (1(Dingin) atau 5(Panas)):</label> <input type="number" id="coffeeTemperature" name="coffeeTemperature" min="1" max="5" value={{ $coffee->coffeeTemperature }}><br>
-
-            <label for="coffeeSweetness">Angka Tingkat Kemanisan (1-5(Sangat Manis)):</label> <input type="number" id="coffeeSweetness" name="coffeeSweetness" min="1" max="5" value={{ $coffee->coffeeSweetness }}><br>
-
-            <label for="coffeeMilkness">Penggunaan Susu (1(Tanpa Susu) atau 5(Dengan Susu)):</label> <input type="number" id="coffeeMilkness" name="coffeeMilkness" min="1" max="5" value={{ $coffee->coffeeMilkness }}><br>
-
-            <label for="coffeeCheapness">Angka Tingkat Harga (1-5(Lebih Mahal)):</label> <input type="number" id="coffeeCheapness" name="coffeeCheapness" min="1" max="5" value={{ $coffee->coffeeCheapness }}><br>
-
-            <label for="coffeeAcidityLevel">Angka Tingkat Keasaman (1-5(Asam)):</label> <input type="number" id="coffeeAcidityLevel" name="coffeeAcidityLevel" min="1" max="5" value={{ $coffee->coffeeAcidityLevel }}><br>
-
-            <label for="coffeeStrengthLevel">Angka Tingkat Kekuatan (1-5(Kuat)):</label> <input type="number" id="coffeeStrengthLevel" name="coffeeStrengthLevel" min="1" max="5" value={{ $coffee->coffeeStrengthLevel }}><br><br>
-
-            <label for="coffeePhoto">Foto Menu:</label> <input type="file" id="coffeePhoto" name="coffeePhoto" accept="image/*"><br>
-            @if ($coffee->coffeePhoto)
-                <img src="{{ asset($coffee->coffeePhoto) }}" style="max-width: 100px; margin-top: 10px;">
-            @endif
-
-            <input type="hidden" id="cafeIdField" name="cafeIdField"><br>
-
+    <div class="container">
+        <div class="addCoffeeForm">
             <br>
+            <h1>Edit Menu Kopi</h1><br>
+            <form action="{{ route('coffee.update', $coffee->id) }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                @method('PUT')
 
-            <input type="submit" value="Submit" class="btn btn-primary">
-            <br><br><br>
-        </form>
+                <input type="hidden" id="cafeId" name="cafeId" value={{ $coffee->cafeId }}>
+
+                {{-- Nama Menu --}}
+                <div class="form-group row">
+                    <label for="coffeeName" class="col-sm-2 col-form-label">Nama menu</label><br>
+                    <div class="col-sm-10">
+                        <input type="text" id="coffeeName" name="coffeeName" class="form-control"
+                            value="{{ $coffee->coffeeName }}">
+                    </div>
+                </div>
+                <p></p>
+
+                {{-- Deskripsi Menu --}}
+                <div class="form-group row">
+                    <label for="coffeeDescription" class="col-sm-2 col-form-label">Deskripsi Menu</label><br>
+                    <div class="col-sm-10">
+                        <input type="text" id="coffeeDescription" name="coffeeDescription"
+                            value="{{ $coffee->coffeeDescription }}" class="form-control">
+                    </div>
+                </div>
+                <p></p>
+
+                {{-- Harga Menu --}}
+                <div class="form-group row">
+                    <label for="coffeePrice" class="col-sm-2 col-form-label">Harga Menu</label>
+                    <div class="col-sm-10">
+                        <input type="number" id="coffeePrice" name="coffeePrice" value="{{ $coffee->coffeePrice }}"
+                            class="form-control">
+                    </div>
+                </div>
+                <hr>
+
+                <input type="hidden" id="coffeeIsBestSeller" name="coffeeIsBestSeller" value="true">
+
+                {{-- Mood --}}
+                <div class="form-group row">
+                    <label for="coffeePreferenceMood" class="col-sm-2 col-form-label">Angka Preferensi Mood
+                        (1-5(Baik))</label>
+                    <div class="col-sm-10">
+                        <input type="number" id="coffeePreferenceMood" name="coffeePreferenceMood" min="1"
+                            max="5" value="{{ $coffee->coffeePreferenceMood }}" class="form-control">
+                    </div>
+                </div>
+
+                {{-- Activity --}}
+                <div class="form-group row">
+                    <label for="coffeePreferenceActivity" class="col-sm-2 col-form-label">Angka Preferensi Kegiatan
+                        (1(Bersantai) atau 5(Melakukan
+                        sesuatu))</label>
+                    <div class="col-sm-10">
+                        <input type="number" id="coffeePreferenceActivity" name="coffeePreferenceActivity" min="1"
+                            max="5" value="{{ $coffee->coffeePreferenceActivity }}" class="form-control">
+                    </div>
+                </div>
+
+                {{-- Temperature --}}
+                <div class="form-group row">
+                    <label for="coffeeTemperature" class="col-sm-2 col-form-label">Suhu Menu (1(Dingin) atau
+                        5(Panas))</label>
+                    <div class="col-sm-10">
+                        <input type="number" id="coffeeTemperature" name="coffeeTemperature" min="1" max="5"
+                            value="{{ $coffee->coffeeTemperature }}" class="form-control">
+                    </div>
+                </div>
+
+                {{-- Sweetness --}}
+                <div class="form-group row">
+                    <label for="coffeeSweetness" class="col-sm-2 col-form-label">Angka Tingkat Kemanisan (1-5(Sangat
+                        Manis))</label>
+                    <div class="col-sm-10">
+                        <input type="number" id="coffeeSweetness" name="coffeeSweetness" min="1" max="5"
+                            value="{{ $coffee->coffeeSweetness }}"class="form-control">
+                    </div>
+                </div>
+
+                {{-- Milk --}}
+                <div class="form-group row">
+                    <label for="coffeeMilkness" class="col-sm-2 col-form-label">Penggunaan Susu (1(Tanpa Susu) atau 5(Dengan
+                        Susu))</label>
+                    <div class="col-sm-10">
+                        <input type="number" id="coffeeMilkness" name="coffeeMilkness" min="1" max="5"
+                            value="{{ $coffee->coffeeMilkness }}" class="form-control">
+                    </div>
+                </div>
+
+                {{-- Price --}}
+                <div class="form-group row">
+                    <label for="coffeeCheapness" class="col-sm-2 col-form-label">Angka Tingkat Harga (1-5(Lebih
+                        Mahal))</label>
+                    <div class="col-sm-10">
+                        <input type="number" id="coffeeCheapness" name="coffeeCheapness" min="1" max="5"
+                            value="{{ $coffee->coffeeCheapness }}" class="form-control">
+                    </div>
+                </div>
+
+                {{-- Acid --}}
+                <div class="form-group row">
+                    <label for="coffeeAcidityLevel" class="col-sm-2 col-form-label">Angka Tingkat Keasaman
+                        (1-5(Asam))</label>
+                    <div class="col-sm-10">
+                        <input type="number" id="coffeeAcidityLevel" name="coffeeAcidityLevel" min="1"
+                            max="5" value="{{ $coffee->coffeeAcidityLevel }}" class="form-control">
+                    </div>
+                </div>
+
+                {{-- Strength --}}
+                <div class="form-group row">
+                    <label for="coffeeStrengthLevel" class="col-sm-2 col-form-label">Angka Tingkat Kekuatan
+                        (1-5(Kuat))</label>
+                    <div class="col-sm-10">
+                        <input type="number" id="coffeeStrengthLevel" name="coffeeStrengthLevel" min="1"
+                            max="5" value="{{ $coffee->coffeeStrengthLevel }}"class="form-control">
+                    </div>
+                </div>
+                <hr>
+
+                <div class="custom-file">
+                    <label for="coffeePhoto" class="custom-file-label col-sm-2 col-form-label">Foto Menu</label>
+                    <input type="file" id="coffeePhoto" name="coffeePhoto" accept="image/*"
+                        class="custom-file-input">
+                    @if ($coffee->coffeePhoto)
+                        <img src="{{ asset($coffee->coffeePhoto) }}" style="max-width: 100px; margin-top: 10px;">
+                    @endif
+                </div>
+
+                <input type="hidden" id="cafeIdField" name="cafeIdField"><br>
+
+                <br>
+
+                <input type="submit" value="Submit" class="btn btn-primary">
+                <br><br><br>
+            </form>
+        </div>
     </div>
-</div>
 @endsection
