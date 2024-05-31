cdec8984ea82adb6951df87d1cfc1c87420f9057
Michael Wijaya Sutrisna <michaelwijayasutrisna2002@gmail.com>
2024-05-31 07:57:21 +0700

    Refactor: Design admin dashboard

--- resources/views/adminDashboardView.blade.php
+++ resources/views/adminDashboardView.blade.php
@@ -1,34 +1,54 @@
 @extends('layout.layout')
 @section('main_content')
+    @php
+        use App\Models\Coffee;
+        use App\Models\Cafe;
+        use Illuminate\Support\Facades\Session;
+
+        $cafeId = Session::get('cafeId');
+        $cafe = Cafe::find($cafeId);
+        $cafeName = $cafe->cafeName;
+
+        $coffees = Coffee::where('cafeId', $cafeId)->get();
+    @endphp
+
+    {{-- Navigation Bar --}}
+    <header class="navbar" id="navbarAdmin">
+        <div class="container">
+            <div class="title-list">
+                <h3 class="titleAdmin"> {{ $cafeName }} Admin</h3>
+            </div>
+            <nav class="nav-listAdmin">
+                <ul>
+                    <li><a class="btn btn-primary" href="/addNewCoffee?cafeId={{ $cafeId }}" id="navButtonAdmin">Tambah
+                            Menu Kopi</a><br>
+                    </li>
+                    <li><a class="btn btn-primary mx-2" href="/" id="navButtonAdmin">Ke Halaman Beranda</a><br></li>
+                    <li>
+                        <form id="logout-form" method="POST" action="/logout">
+                            @csrf
+                            <button type="submit" class="btn btn-danger" id="logOutButton">Log Out</button>
+                        </form>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
     <div class="container">
         <div class="adminDashboardMainContainer">
-            @php
-                use App\Models\Coffee;
-                use App\Models\Cafe;
-                use Illuminate\Support\Facades\Session;
-
-                $cafeId = Session::get('cafeId');
-                $cafe = Cafe::find($cafeId);
-                $cafeName = $cafe->cafeName;
-
-                $coffees = Coffee::where('cafeId', $cafeId)->get();
-            @endphp
-
-            <h1>{{ $cafeName }} Admin</h1>
-            <br>
-
-            <div class="functionButtonAdminAdd">
-                <a class="btn btn-primary" href="/addNewCoffee?cafeId={{ $cafeId }}">Tambah Menu Kopi</a><br>
-                <a class="btn btn-primary mx-5" href="/">Ke Halaman Beranda</a><br>
-                <form id="logout-form" method="POST" action="/logout">
-                    @csrf
-                    <button type="submit" class="btn btn-warning">Log out</button>
-                </form>
-            </div>
-            <br>
-
-            <table class="d-flex justify-content-center">
+            <table class="d-flex justify-content-center table" id="adminTable">
                 <tbody>
+                    <tr>
+                        <th colspan="5" id="tableTitle">Daftar Menu</th>
+                    </tr>
+                    <tr>
+                        <th scope="col">Foto Menu</th>
+                        <th scope="col">Nama Menu</th>
+                        <th scope="col">Deskripsi Menu</th>
+                        <th scope="col">Harga Menu</th>
+                        <th scope="col">Tombol Aksi</th>
+                    </tr>
                     @if (count($coffees) == 0)
                         <p style="margin-top: 150px">Tidak Ada Menu</p>
                     @endif
